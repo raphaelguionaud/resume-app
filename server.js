e279738cf2e286098d0ef133294b111dce67eff1
raphaelguionaud <raphael.guionaud@outlook.com>
2022-11-26 18:28:53 +0000

    removed <>

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/<newresume>'));
+app.use(express.static(__dirname + '/dist/newresume'));
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/dist/<newresume>/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/newresume/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
